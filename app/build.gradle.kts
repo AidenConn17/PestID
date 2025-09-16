325c05923204c8097e54f689626c1ba3072190f5
AidenConn17 <aidenconn14@gmail.com>
2025-09-16 10:35:36 -0400

    Please stop crashing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,15 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.camera:camera-core:1.5.0")
+    implementation("androidx.camera:camera-camera2:1.5.0")
+    implementation("androidx.camera:camera-lifecycle:1.5.0")
+    implementation("androidx.camera:camera-view:1.5.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.camera.lifecycle)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/pestid/MainActivity.java
+++ app/src/main/java/com/example/pestid/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.pestid;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.Preview;
@@ -16,18 +23,31 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     PreviewView previewView;
+    ImageCapture imageCapture = new ImageCapture.Builder().setTargetRotation(
+            getScreenRotationInDegrees(this))
+            .build();
+    Camera camera;
+    Executor cameraExecutor;
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+//            cameraExecutor = getMainExecutor();
+//        }
         cameraProviderFuture =ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try{
@@ -49,7 +69,28 @@ public class MainActivity extends AppCompatActivity {
         previewView = findViewById(R.id.previewView);
 
         button.setOnClickListener(v -> {
+            String name = new SimpleDateFormat("yyyMMddHHmmss", Locale.US).format(new Date());
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
 
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+            }
+
+            ImageCapture.OutputFileOptions outputFileOptions =
+                    new ImageCapture.OutputFileOptions.Builder(this.getContentResolver(), MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build();
+//            imageCapture.takePicture(outputFileOptions, cameraExecutor, new ImageCapture.OnImageSavedCallback() {
+//                @Override
+//                public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+////                    Toast.makeText();
+//                }
+//
+//                @Override
+//                public void onError(ImageCaptureException error) {
+//
+//                }
+//            });
         });
     }
 
@@ -59,7 +100,36 @@ public class MainActivity extends AppCompatActivity {
         CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageCapture, preview);
     }
 
+    int getScreenRotationInDegrees(Context context){
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        Display display;
+        int rotationConstant;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            display = context.getDisplay();
+        } else {
+            display = wm.getDefaultDisplay();
+        }
+
+        if(display != null){
+            rotationConstant = display.getRotation();
+        } else {
+            rotationConstant = Surface.ROTATION_0;
+        }
+
+        switch(rotationConstant){
+            case Surface.ROTATION_0:
+                return 0;
+            case Surface.ROTATION_90:
+                return 90;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_270:
+                return 270;
+        }
+
+        return 0;
+    }
 }
